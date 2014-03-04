cbd2c76a9547b30d43a101e2f4c109eba0245949
Mark Mossberg <mark.mossberg@gmail.com>
2014-03-04 00:59:05 -0500

    add arg parsing, config file processing

--- unlock.py
+++ unlock.py
@@ -11,6 +11,8 @@ import urllib
 import urllib2
 import httplib
 import os.path
+import getpass
+import argparse
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://huskycardcenter.neu.edu'
@@ -21,10 +23,12 @@ def scrape(path):
     path = BASE_URL + path
     page = urllib2.urlopen('{}'.format(path))
     cookie = page.headers.get('set-cookie').split(';')[0]
-    print '[i] Got cookie: {}'.format(cookie)
+    if args.verbose:
+        print '[i] Got login cookie: {}'.format(cookie)
     soup = BeautifulSoup(page.read())
     sesstok = soup.find('script').text.split()[-1][1:-2]
-    print '[i] Got sesstok: {}'.format(sesstok)
+    if args.verbose:
+        print '[i] Got login session token: {}'.format(sesstok)
     return sesstok, cookie
 
 def generate_headers(cookie):
@@ -44,27 +48,64 @@ def get_config():
     """
 
     config_path = os.path.expanduser('~/.unlock.json')
-    with open(config_path) as f:
-        config = json.load(f)
+    try:
+        with open(config_path) as f:
+            config = json.load(f)
+    except IOError:
+        print '[!] You need a ~/.unlock.json config file. \
+Create one now (y/n)?',
+        input = raw_input()
+        if input.lower() == 'y':
+            config = create_config()
+        else:
+            print '[-] Exiting'
+            sys.exit(1)
     return config
+
+def get_args():
+    """ Parse arguments and return dictionary. """
+
+    parser = argparse.ArgumentParser(description="Unlock your Northeastern \
+                                     University on campus, NFC enabled, dorm \
+                                     door via the command line.")
+    parser.add_argument('-r', '--room', help='unlock your actual room door \
+                        instead of your apartment\'s door',
+                        action='store_true')
+    parser.add_argument('-v', '--verbose', help='display detailed info',
+                        action='store_true')
+    return parser.parse_args()
+
+
+def create_config():
+    """ Generate ~/.unlock.json config file. """
+
+    print """[+] This will generate a ~/.unlock.json file for you, which is
+    necessary for the script to authenticate you into huskycardcenter.neu.edu"""
+    u = raw_input('myNEU username: ')
+    p = getpass.getpass('myNEU password: ')
+    config_path = os.path.expanduser('~/.unlock.json')
+    with open(config_path, 'w') as f:
+        json.dump({'USER': u, 'PASS': p}, f, indent=4)
+    print '[+] Config file created'
+    print '[-] Exiting'
 
 
 def main():
+    global args
+    args = get_args()
     config = get_config()
 
-    print '[+] Getting session token'
+    if args.verbose: print '[+] Starting'
 
     sesstok, cookie = scrape('/login/ldap.php')
     post_body = urllib.urlencode({'__sesstok': sesstok, 'user': config['USER'],
                                   'pass': config['PASS']})
 
-    print '[+] Logging in'
-
     login = httplib.HTTPSConnection('huskycardcenter.neu.edu')
     login.request('POST', '/login/ldap.php', post_body, generate_headers(cookie))
     r1 = login.getresponse()
     newcookie = r1.getheader('set-cookie').split(';')[0]
-    print '[i] New cookie: {}'.format(newcookie)
+    if args.verbose: print '[i] Got user cookie: {}'.format(newcookie)
 
     headers = generate_headers(newcookie)
     test = httplib.HTTPSConnection('huskycardcenter.neu.edu')
@@ -72,32 +113,26 @@ def main():
     r2 = test.getresponse()
 
     if 'Welcome to CS Gold WebCard Center' in r2.read():
-        print '[+] Authenticated Successfully!'
+        if args.verbose: print '[+] Login Successful!'
     else:
-        print '[-] Authentication Failed'
+        print '[-] Login Failed!'
+        print '[-] Exiting'
         sys.exit()
 
-    print '[+] Getting openmydoor.php session token'
+    if args.verbose: print '[+] Getting door session token'
     openmydoor = httplib.HTTPSConnection('huskycardcenter.neu.edu')
     openmydoor.request('GET', '/student/openmydoor.php', headers=headers)
     r3 = openmydoor.getresponse()
     soup = BeautifulSoup(r3.read())
     door_sesstok = soup.find('script').text.split()[-1][1:-2]
-    print '[i] Got sesstok: {}'.format(door_sesstok)
-    print '[?] Enter door to unlock (1 - Apt, 2 - Room):',
-    choice = raw_input()
+    if args.verbose: print '[i] Got sesstok: {}'.format(door_sesstok)
 
-    if choice == '1':
-        open_body = urllib.urlencode({'__sesstok': door_sesstok,
-                                      'doorType': 1, 'answeredYes': 'yes'})
-    elif choice == '2':
-        open_body = urllib.urlencode({'__sesstok': door_sesstok,
-                                      'doorType': 2, 'answeredYes': 'yes'})
-    else:
-        print '[-] Invalid choice'
-        sys.exit()
+    room = 2 if args.room else 1
+    open_body = urllib.urlencode({'__sesstok': door_sesstok,
+                                  'doorType': room, 'answeredYes': 'yes'})
 
-    print '[+] Unlocking door! (it may take up to 10 seconds, wait for it)'
+    if args.verbose: print '[+] Unlocking door! (it may take up to 10 seconds,\
+ wait for it)'
     boom = httplib.HTTPSConnection('huskycardcenter.neu.edu')
     boom.request('POST', '/student/openmydoor.php', open_body, headers)
 
